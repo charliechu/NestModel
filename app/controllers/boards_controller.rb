97550574392fc8f14a7a6ce09eeb8f5ff5ffafb8
charliechu <charliechu@techbang.com.tw>
2011-09-08 12:16:50 +0800

    Create board id to posts, NEST RESTFUL CRUD

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,2 +1,44 @@
 class BoardsController < ApplicationController
+  before_filter :find_board, :only => [:edit, :show, :update, :destroy]
+  def index
+    @boards = Board.all
+  end
+  
+  def new
+    @board = Board.new
+  end
+  
+  def create
+    @board = Board.new(params[:board])
+    if @board.save
+      redirect_to boards_path
+    else
+      render :action => "new"
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @board.update_attributes(params[:board])
+      redirect_to board_path(@board)
+    else
+      render :action => "edit"
+    end
+  end
+  
+  def show
+  end
+  
+  def destroy
+    @board.destroy
+    redirect_to boards_path
+  end
+  
+  protected
+  
+  def find_board
+    @board = Board.find(params[:id])    
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,52 @@
 class PostsController < ApplicationController
+  before_filter :find_board, :only => [:index, :new, :create, :edit, :update, :destroy, :show]
+  before_filter :find_post, :only => [:show, :edit, :update, :destroy]
+  
+  def index
+    @posts = @board.posts.all
+  end
+  
+  def new
+    @post = @board.posts.new
+  end
+  
+  def create
+    @post = @board.posts.new(params[:post])
+    if @post.save
+      redirect_to board_posts_path
+    else
+      render :action => "new"
+    end
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    if @post.update_attributes(params[:post])
+      redirect_to board_post_path(@board ,@post)
+    else
+      render :action => "edit"
+    end
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    @post.destroy
+    redirect_to board_posts_path
+  end
+  
+  protected
+  
+  def find_board
+    @board = Board.find(params[:board_id])
+  end
+  
+  def find_post
+    @post = @board.posts.find(params[:id])
+  end
 end
