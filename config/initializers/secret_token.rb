d01381b6d086979c1baa3a6bad62931aa49b7550
charliechu <charliechu@techbang.com.tw>
2011-09-08 10:27:47 +0800

    First commit, .gitignore database.yml.exapmle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nestmodel::Application.config.secret_token = '9fde44ce39d4db77bed583b9d80f7a0342fa4bc4259f0c97a81967fd2fb39085c0e11d2eb175dc40c0d5bb2ccc82ee98ad2e5e3efc3db3b97dae053e7364bc12'
